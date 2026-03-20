7e24880a34d09cf18bf6359b3182600d7bbb14dc
Jhesua Machado <jhesua_machado@hotmail.com>
2026-03-20 10:48:53 -0300

    Iniciando q2 em python

--- ex1.py
+++ ex1.py
@@ -1,5 +1,6 @@
 import numpy as np
 import timeit
+from time import perf_counter, perf_counter_ns
 
 sample_A = np.random.randint(1, 10, (2,2))
 sample_B = np.random.randint(1, 10, (2,2))
@@ -18,25 +19,45 @@ def mat_prod(A: np.matrix, B: np.matrix) -> np.matrix:
     return C
 
 # Shapes for test
-A_sps = ((2,10),(15,10),(6,10),(500, 10), (500, 500), (400, 500))
-B_sps = ((10,5),(10,2),(10,6),(10, 300), (500, 500), (500, 500))
+A_shps = ((1,1), (2,10),(15,10),(6,20),(500, 113), (500, 500), (400, 500), (1000, 1000))
+B_shps = ((1,1), (10,5),(10,2),(20,6),(113, 300), (500, 500), (500, 500), (1000, 1000))
+times = (10000, 9000, 8000, 7000, 10, 10, 10, 2)
 
-# Function for measure the time of item B, C and D
-def time_measure(function):
-    print(function.__name__, ": \n")
-    for shape_a, shape_b in zip(A_sps, B_sps):
+data = {"A":[], "B":[], "times": times}
+
+for shape_a, shape_b in zip(A_shps, B_shps):
         A = np.random.randint(0,1000, shape_a)
         B = np.random.randint(0,1000, shape_b)
+
+        data["A"].append(A)
+        data["B"].append(B)
+
+# Function for measure the time of item B, C and D
+def time_matrix_product_measure(function):
+    print(function.__name__, ": \n")
+    
+    for A, B, times in zip(data["A"], data["B"], data["times"]):
 
         # heating
         function(A, B)
 
-        tempo = timeit.timeit(lambda: function(A, B), number=3)
-        print(tempo)
+        #time
+        time = []
+
+        for _ in range(times):
+            inicio = perf_counter_ns()
+            function(A, B)
+            fim = perf_counter_ns()
+
+            # calculamos o tempo agregado para depois fazer uma media
+            time.append(fim - inicio)
+
+        print("dp:", np.std(time), "m:", sum(time)/times)
+        
     print()
 
 # Item B)
-# time_measure(mat_prod)
+#time_matrix_product_measure(mat_prod)
 
 # Item C)
 def mat_prod_dot(A, B):
@@ -50,7 +71,43 @@ def mat_prod_dot(A, B):
 
     return C
 
-time_measure(mat_prod_dot)
+# time_matrix_product_measure(mat_prod_dot)
 
 # Item D)
-time_measure(np.matmul)
+# time_matrix_product_measure(np.matmul)
+
+C = np.array([2,4,6,8])
+A = np.array([1,2,3,4])
+B = np.array([3,7,9,0])
+
+
+def solve_tridiag(A:np.ndarray, b:np.ndarray) -> np.ndarray:
+    """resolve um sistema tridiagonal
+
+    Parameters
+    ----------
+    A : np.ndarray
+        Uma matriz quadrada invertivel
+    b : np.ndarray
+        resultado que queremos chegar
+
+    Returns
+    -------
+    np.ndarray
+        solução do sistema
+    """
+    A_c = [A[0]]
+
+
+    for i in range(1, len(A), 1):
+         a_i = A[i] - (C[i]/A_c[i-1])*B[i-1]
+         A_c.append(a_i)
+
+    print(A_c)
+
+
+
+solve_tridiag(A, B)
+         
+
+
