e1177f18cae9099ee0bcd2a44b2313f1a182351e
Jhesua Machado <jhesua_machado@hotmail.com>
2026-03-25 00:07:43 -0300

    finalizando q1 projeto 1 em python

--- ex1_.py
+++ ex1_.py
@@ -1,4 +1,6 @@
 import numpy as np
+import pandas as pd
+from time import perf_counter
 
 ################################ Questão 1 - A ################################
 
@@ -15,19 +17,48 @@ def mat_prod(A: np.matrix, B: np.matrix) -> np.matrix:
     return C
 
 ################################ Questão 1 - B ################################
+# temos 3 classes de teste e em cada categoria de teste 2 tipos de teste
+# então ao total realizaremos 6 testes e mediremos seus tempos
 
-# TODO: Criar casos de teste 
-#       - Neste item os casos de teste são comparados entre si
-#       - Usar apropiadamente as funcoes de medicao de tempo para evitar erros
-#       de medida, algumas das praticas são:
-#           - Executar 1, 2 ou um numero pequeno de vezes para compilar
-#           - O tempo de execução muda de computador para computador então é
-#           interessante fazer uma razão para dizer quanto mais rapido é um ca-
-#           so em relação a outro
-#           - Quanto o codigo é simples, é interessante executar varias vezes
-#           para evitar flutuações de processos em segundo plano e então fazer
-#           uma media dos tempos.
+def medir_tempo(func, *args, repeat=10):
+    tempos = []
+    for _ in range(repeat):
+        inicio = perf_counter()
+        func(*args)
+        fim = perf_counter()
+        tempos.append(fim - inicio)
+    return np.mean(tempos)
 
+tests_cases ={
+    "m < n, pequena (15, 10, 30)":(15, 10, 30),    
+    "m > n, pequena (30, 10, 15)":(30, 10, 15),    
+    "m = n, pequena (30, 30, 30)":(30, 30, 30), 
+    "m < n, grande (300, 200, 500)":(300, 200, 500), 
+    "m > n, grande (500, 200, 300)":(500, 200, 300),
+    "m = n, grande (500, 500, 500)":(500, 500, 500), 
+}
+
+def test_prod(m,k,n, func):
+    np.random.seed(42)
+    A = np.random.randn(m, k)
+    B = np.random.randn(k, n)
+
+    # Esquentamento
+    mat_prod(A, B)
+    mat_prod(A, B)
+
+    tempo = medir_tempo(func, A, B)
+    return tempo
+
+def run_cases(func):
+    tempos = []
+    for nome, cada_caso in tests_cases.items():
+        m, k, n = cada_caso
+        tempo = test_prod(m, n, k, func)
+        tempos.append(tempo)
+    return tempos
+
+tempos_mat_prod = run_cases(mat_prod)
 ################################ Questão 1 - C ################################
 
 def mat_prod_dot(A, B):
@@ -41,11 +72,43 @@ def mat_prod_dot(A, B):
 
     return C
 
-# TODO: Executar novamente os casos de teste anteriores mas agora com
-#       mat_prod_dot
+tempos_mat_prod_dot = run_cases(mat_prod_dot)
+# A complexidade algoritmica diminuiu logo diminui o tempo de execução dos
+# testes anteriores
 
 ################################ Questão 1 - D ################################
 
-# TODO: Executamos novamente os casos de teste anteriores com np.matmul
-#       - Como queremos comparar cada funcao aos casos de teste anteriores é
-#       interessante fazer todas as razoes em relacao a funcao mat_prod
+tempos_matmul = run_cases(np.matmul)
+
+t_mat_prod = np.array(tempos_mat_prod)
+t_mat_prod_dot = np.array(tempos_mat_prod_dot)
+t_matmul = np.array(tempos_matmul)
+
+compare = np.c_[
+    np.ones_like(t_mat_prod),
+    t_mat_prod/t_mat_prod_dot,
+    t_mat_prod/t_matmul
+]
+
+exact = np.c_[
+    t_mat_prod,
+    t_mat_prod_dot,
+    t_matmul
+] 
+
+comp = pd.DataFrame(
+    compare,
+    tests_cases,
+    columns=["mat_prod", "mat_prod_dot", "matmul"]
+)
+
+ex = pd.DataFrame(
+    exact,
+    tests_cases,
+    columns=["mat_prod", "mat_prod_dot", "matmul"]
+)
+
+print("Quanto as outras funcoes são melhores?")
+print(comp)
+print("Valores exatos de tempo")
+print(ex)
